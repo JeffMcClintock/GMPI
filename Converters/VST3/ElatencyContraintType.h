5cddc26ae57af4836088df69a61d4ddb61cdc289
Jeff MᶜClintock <jef@synthedit.com>
2023-05-02 11:14:45 +1200

    chore(gm) : Remove sample plugins to their own repo

--- Converters/VST3/ElatencyContraintType.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-enum class ElatencyContraintType
-{
-	Off, Constrained, Full
-};
--- Converters/VST3/se_types.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <limits.h>
-//#include "mp_api.h"
-
-typedef int64_t timestamp_t;
-const timestamp_t SE_TIMESTAMP_MAX = INT64_MAX;
